dc570375aed8172a67f0e2da403601e40ce55d6d
Alexander Standke <xanderstrike@gmail.com>
2014-02-03 13:16:02 -0800

    add transFact

--- matrix4.h
+++ matrix4.h
@@ -278,7 +278,26 @@ inline Matrix4 normalMatrix(const Matrix4& m) {
 }
 
 inline Matrix4 transFact(const Matrix4& m) {
-  // TODO
+  Matrix4 trans;
+
+  trans(0,0) = 1;
+  trans(0,1) = 0;
+  trans(0,2) = 0;
+  trans(0,3) = m(0,3);
+  trans(1,0) = 0;
+  trans(1,1) = 1;
+  trans(1,2) = 0;
+  trans(1,3) = m(1,3);
+  trans(2,0) = 0;
+  trans(2,1) = 0;
+  trans(2,2) = 1;
+  trans(2,3) = m(2,3);
+  trans(3,0) = 0;
+  trans(3,1) = 0;
+  trans(3,2) = 0;
+  trans(3,3) = 1;
+
+  return trans;
 }
 
 inline Matrix4 linFact(const Matrix4& m) {
